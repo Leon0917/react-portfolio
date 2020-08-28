5c5ada0cbcdd8212f0e733a9c2bd3dee1c4f56a3
Leon Carnagie <leonfross@gmail.com>
2020-08-27 21:45:48 -0700

    Modified app.js to fix routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -9,28 +9,20 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-     
-     <BrowserRouter>
-     <Header/>
-     <Switch>
-       <Route exact ="/">
-           <Portfolio />
-       </Route>
-       <Route exact ="/about">
-           <About />
-       </Route>
-       <Route exact ="/contact">
-           <Contact />
-       </Route>
-       <Route exact ="/portfolio">
-           <Portfolio />
-       </Route>
-
-     </Switch>
-     </BrowserRouter>
-     <Footer/>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path={["/", "/portfolio"]} component={Portfolio}/>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
 
 export default App;
+         
+
+           
